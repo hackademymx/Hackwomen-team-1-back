783b2afc9331251e6e7987c7bb6e6f654f2d1833
silvaanis8 <ada_hack30@hackwomen.dev>
2023-03-05 23:10:57 -0300

    Añadiendo archivo de imagen

--- lugares_seguros/lugares_seguros/settings/base.py
+++ lugares_seguros/lugares_seguros/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +145,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ]
 }
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- lugares_seguros/places/views.py
+++ lugares_seguros/places/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from.models import Place
+from.serializers import PlaceSerializer
 
 # Create your views here.
+
+
+class PlaceAPIView(APIView):
+
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request):
+        print(request.data)
+        try:
+            file = request.data['image']
+            request.data['image'] = file
+        except KeyError:
+            file = None  
+        serializer = PlaceSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
