66b66d9c12885e27ea98e476b0d52e6aaa8d93fb
silvaanis8 <ada_hack30@hackwomen.dev>
2023-03-08 00:46:32 -0300

    Creación app usuarios

--- lugares_seguros/users/serializers.py
+++ lugares_seguros/users/serializers.py
@@ -13,3 +13,9 @@ class RegisterUserSerializer(serializers.Serializer):
         except:
             raise serializers.ValidationError({"error":"Usuario con este username ya existe."})
         return user 
+    
+
+class LoginUserSerializer(serializers.Serializer):
+    
+    username = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=50)
--- lugares_seguros/users/urls.py
+++ lugares_seguros/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, LoginUserView
 
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view())
+    path('register/', RegisterUserView.as_view()),
+    path('login/', LoginUserView.as_view()),
 ]
