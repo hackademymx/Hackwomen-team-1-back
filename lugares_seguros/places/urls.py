0a5b675c01fec1193f2e43b374a4ac5f0c3e4e5c
miriam-moztap <ada_hack16@hackwomen.dev>
2023-03-14 23:32:43 -0600

    se eliminan errores de comentarios extra

--- lugares_seguros/places/urls.py
+++ lugares_seguros/places/urls.py
@@ -1,16 +1,16 @@
 from django.urls import URLPattern, path
-<<<<<<< HEAD
+
 from .views import PlaceAPIDeleteView
 from places import views
 
 urlpatterns= [
     path('places/',PlaceAPIDeleteView.as_view()),   
 ]
-=======
+
 from .views import PlaceAPIView
 
 
 urlpatterns = [
     path('', PlaceAPIView.as_view())
     ]
->>>>>>> b90613e8ab3811348bf34a27e57a3f3b724c9d1f
+
