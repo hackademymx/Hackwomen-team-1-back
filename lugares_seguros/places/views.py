7eb1f7a04b174a4a0100704947aeee1bebfa760e
miriam-moztap <ada_hack16@hackwomen.dev>
2023-03-14 23:24:25 -0600

    se remueve código delete

--- lugares_seguros/places/views.py
+++ lugares_seguros/places/views.py
@@ -7,33 +7,6 @@ from .models import Place
 from.serializers import PlaceSerializers
 
 # Create your views here.
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Place
-from .serializers import PlaceSerializer
-from rest_framework.parsers import MultiPartParser, FormParser
-
-places = Place.objects.all()
-    serializer = PlaceSerializer(places,many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-   
-    
-
-class PlaceAPIDeleteView(APIView):
-    
-    parser_classes= (MultiPartParser, FormParser)
-    
-    def patch(self, request, id):
-        place = Place.objects.filter(id = id).first()
-        if place is None:
-            return Response({'error': 'Bad request'}, status = status.HTTP_400_BAD_REQUEST)
-        place.delete()
-        return Response({'message': 'lugar eliminado correctamente'}, status = status.HTTP_200_OK)
-
 
 class PlaceAPIView(APIView):
 
