050cdab7eec95da8dd257579985a391bb934c3f9
miriam-moztap <ada_hack16@hackwomen.dev>
2023-03-15 13:30:15 -0600

    se aceptan cambios

--- lugares_seguros/coments/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coments'
--- lugares_seguros/coments/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Coment
-
-class ComentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coment
-        fields = '__all__'
-        
